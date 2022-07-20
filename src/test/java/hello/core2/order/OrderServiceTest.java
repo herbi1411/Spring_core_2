778f44553f566c13457a9dedcf6fc81817d463e5
YoonMinSoo <minsoo9615@naver.com>
2022-07-20 13:46:48 +0900

    discount, orderService

---
+++ src/test/java/hello/core2/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core2.order;
+
+import hello.core2.member.Grade;
+import hello.core2.member.Member;
+import hello.core2.member.MemberService;
+import hello.core2.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        long memberId = 1L;
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
